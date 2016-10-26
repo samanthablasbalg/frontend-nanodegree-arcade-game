a20e655feb4add51f09e0617daa70b2a6384bb20
samanthablasbalg <blasbalgs@gmail.com>
2016-10-26 11:51:34 -0400

    modify engine.js so map will render and add player to start position in app.js

--- js/app.js
+++ js/app.js
@@ -24,11 +24,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 375;
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+allEnemies = [];
+var player = new Player;
 
 
 
